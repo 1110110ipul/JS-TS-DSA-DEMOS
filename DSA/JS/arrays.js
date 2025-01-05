fe6af8f1255073276dc0ac64885aae7cfc0e7502
https://github.com/1110110ipul <gvipul890@gmail.com>
2025-01-05 19:34:44 +0530

    day 2 js problem statement

--- DSA/JS/arrays.js
+++ DSA/JS/arrays.js
@@ -11,5 +11,21 @@ function transformToArray(arr){
 
 let transformToArrayNumber = [1,2,3,4,5,6,7,8,9];
 const resultDay1 = transformToArray(transformToArrayNumber);
-console.log(`Array: [${transformToArrayNumber}], Result: ${resultDay1}`);
+// console.log(`Array: [${transformToArrayNumber}], Result: ${resultDay1}`);
+
+// day 2
+
+// Problem Statement
+// Given an array of integers, return a new array with only the unique elements, removing any duplicates.
+
+function removeDuplicates(arr){
+    return [...new Set(arr)];
+}
+
+// Test case
+const inputArray = [1, 2, 2, 3, 4, 4, 5];
+const result = removeDuplicates(inputArray);
+
+console.log(result); // Output: [1, 2, 3, 4, 5]
+
 
